e381d38d2af0d08ceceede104fc8b7ed822a2ed4
WWWCourses <ivawebcourses@gmail.com>
2019-06-03 21:18:05 +0300

    Lab1: 03.06.2019 DONE: http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/JS_BasicSyntax_1/JS_BasicSyntax_1.html#/5/4 NEXT: next TODO: check todos in files

---
+++ lab20190603/variablesDemo.js
@@ -0,0 +1,20 @@
+// деклариране на променливата x
+var x;
+
+// инициализиране на променливата x
+x = 5;
+
+// дефиниране на променливата y:
+var y = 9
+
+// NEVER DO THAT, though it works!
+z = 8;
+console.log(z + 10);
+
+// variables naming
+var $ = 'the dolar sign';
+console.log($);
+
+// case-sensitive
+var name = "iva";
+var Name = "popova";
