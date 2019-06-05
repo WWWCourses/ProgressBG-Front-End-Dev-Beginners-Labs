cb8baf3dd3e290f4886601bb7d18e74fe57c6144
WWWCourses <ivawebcourses@gmail.com>
2019-06-05 20:36:38 +0300

    lab2: HW set

---
+++ lab20190605/dataTypes.js
@@ -0,0 +1,27 @@
+// number
+var userAge = 23;
+// console.log( typeof userAge );
+
+// string
+var userName = 'ivan';
+var userSurName = "Ivanov";
+
+
+// console.log('userName');
+// console.log(userName);
+
+var jonPubName = "Jon's pub";
+var ivanPubName = 'Jon\'s pub';
+
+
+// template string
+var poema = `line 1\nline 2  
+line 3
+line 4`;
+console.log(poema);
+
+
+poema
+
+
+
---
+++ lab20190605/programStructuring.js
@@ -0,0 +1,23 @@
+var userName='Ada';
+var userAge = 23;
+
+// HW: make it without Template Strings
+console.log(`Здравей ${userName}, ти си на ${userAge} години!`);
+// Здравей Ada, ти си на 23 години!
+
+
+console.log('userName - ' + userName); // userName - Ada
+console.log(`userName - ${userName}`); // userName - Ada
+
+console.log(`${2+3}`);
+console.log('2+3'); 
+console.log(2+3);
+
+console.log(`${aaaa'}`);
+
+console.log("${aaaa'}");
+
+
+
+
+// userName - Ada
