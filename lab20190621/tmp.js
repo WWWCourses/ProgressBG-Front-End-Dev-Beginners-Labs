2f97af78e87273e0f75e3a04a12aa223675d0e40
WWWCourses <ivawebcourses@gmail.com>
2019-06-21 23:23:12 +0300

    Day10[END]: 21.06.2019 DONE: timers; sample HTML/CSS/JS components TODO: finish imageSlider demowritten in labs; send mail with code link

---
+++ lab20190621/tmp.js
@@ -0,0 +1,9 @@
+let colors = ['red', 'green', 'blue'];
+
+for (var i = 0; i < colors.length; i++) {
+    console.log('====================================');
+    console.log(`i = ${i}`);
+    console.log('====================================');
+};
+
+console.log(`i = ${i}`); //
