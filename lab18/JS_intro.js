fc6334a7771d632eb8d51d84a74e858432afe9a2
WWWCourses <ivawebcourses@gmail.com>
2019-12-14 13:33:09 +0200

    Lab18: 14.12.2019 DONE: till http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/JS_BasicSyntax_1/JS_BasicSyntax_1.html#/7/5 NEXT: next

---
+++ lab18/JS_intro.js
@@ -0,0 +1,126 @@
+"use strict";
+var a=3, b=4,c=5;
+// var a = 3;
+// var b = 4;
+// var c = 5;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// if( a**2 + b**2 === c**2 ){
+//     console.log("Yes it is Pythagorean!");
+// }
+
+// console.log(`~~~~~~~~~~~~~~~~~~~`);
+
+// var user_age = 12;
+
+// var user_first_name = 'Ada';
+// var userlastname = 'Byron';
+
+console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+
+// var $ = 3000;
+// console.log($);
+
+// var _ = 'a';
+// console.log(_);
+
+
+// variables naming conventions:
+// snake case (Python)
+var user_first_name = "Ada";
+console.log(user_first_name);
+
+var user$first$name = "Iva";
+console.log(user$first$name);
+
+var UserFirstName = "Petar";
+console.log(UserFirstName);
+
+// JS convention
+var userFirstName = "Petar";
+console.log(userFirstName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lab18/dataTypes.js
@@ -0,0 +1,42 @@
+// console.log( 3 + 3);
+// console.log( 3 + "3" );
+
+// integer number:
+// var x = 234;
+// console.log(x);
+
+
+// // float number:
+// x = 3.14;
+// console.log(x);
+
+// console.log( 3,14  + 10); // wrong
+// console.log( 3.14  + 10);
+
+// positive && negative:
+console.log( -3.14 + 0.14); // -3
+console.log( 0.14 - 3.14);  // -3
+console.log( 0.14 - +3.14); // -3
+
+console.log( -    5 + 6); // 1
+console.log( -5 + 6); // 1
+console.log( - (5 + 6) ); // -11
+
+// 5 milion
+console.log(5000000);
+console.log(5.e6);
+
+console.log( 30000000000000001223 * 300000000000000000000 );
+
+
+// strings
+var address1 = "Ada's house";
+var address2 = 'Ada\'s house';
+var address3 = 'Ada     s house';
+console.log(address1);
+console.log(address2);
+console.log(address3);
+
+
+
+
