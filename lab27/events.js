f0b4a1cbc01058721861bddb32624264727ba97d
WWWCourses <ivawebcourses@gmail.com>
2020-01-27 23:19:03 +0200

    Lab27: 27.01.2020 DONE: JS_StylingElements NEXT: next

---
+++ lab27/events.js
@@ -0,0 +1,13 @@
+const  inputNode =
+
+function clickHandler(){
+
+}
+
+function countSymbols(str) {
+    return str.length
+}
+let symbols = countSymbols("1234dsdsdsds");
+console.log(symbols);
+
+// clickHandler();
