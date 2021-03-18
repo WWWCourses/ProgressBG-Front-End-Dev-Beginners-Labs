cfdc4b698a21416c3617c153d4c289c2bbb5b108
WWWCourses <ivawebcourses@gmail.com>
2021-03-18 12:14:27 +0200

    unify students folder with other branches

---
+++ .bin/checkStudentsGitHubInBrowser.sh
@@ -0,0 +1,7 @@
+#TODO: get URLs from hidden conf
+google-chrome --new-window \
+https://github.com/bntsv \
+https://github.com/atanas-atanasov2 \
+https://github.com/kamcveto \
+https://github.com/Deshov \
+https://github.com/StGochew97 \
