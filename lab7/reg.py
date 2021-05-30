323025a351f0faa04bd3d007438ec1c86318df36
WWWCourses <ivawebcourses@gmail.com>
2021-05-30 13:33:02 +0300

    Lab7: 30.05.2021 DONE: HTML Forms NEXT: CSS Intro

---
+++ lab7/reg.py
@@ -0,0 +1,10 @@
+# 1. Get the data from server
+# 2. userName = "iva", pass = 123
+# 3. INSERT into USERS(userName="iva", pass="")
+
+
+DataBase:
+USERS:
+id	| userName	| pass |
+1 	|      iva	| 123 	|
+2 	|     pesho	| 8765 	|
---
+++ mysite.com/back-end/login.py
@@ -0,0 +1,23 @@
+# This is a fake back-end program file
+
+# userName = "Iva"
+# userPass = "123"
+# age=222
+# gender=f
+# drink = beer,coca-cola
+# town=Sofia
+
+
+http://127.0.0.1:5500
+
+PATH: /back-end/hfddsdskjs.php
+DATA: uname=iva&pass=123&age=222
+
+INSERT into USERS (userName=userName,userPassword=userPass,age=age)
+
+
+USERS:
+
+userName 	| userPassword  | age | Gender | drink			|  Town
+Iva 		| 1234			|     |   f    | beer,coca-cola |  Sofia
+Pesho		| 1fd4			|     |   m
