9fcef75f71bdd21c2a951d9ca105970f091560f8
WWWCourses <ivawebcourses@gmail.com>
2021-07-04 13:36:43 +0300

    Lab17: 04.07.2021 DONE: till http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/JS_BasicSyntax_1/JS_BasicSyntax_1.html#/Variables

---
+++ lab17/intro.js
@@ -0,0 +1,34 @@
+let btn = document.querySelector('button');
+let input = document.querySelector('[type="number"]');
+btn.addEventListener('click', function () {
+	let x = input.value;
+	// let x = 4;
+	console.log(x);
+	let res = x**2;
+	console.log(res);
+})
+
+
+
+
+
+
+
+// RAM:
+// intro.js
+
+// let res = x**2;
+// console.log(res);
+
+
+
+
+
+
+// 	044343: 0100
+// 0653773: 0010
+// 8843834: 1000
+
+// Global
+// 044343=>x
+// 8843834=>res
