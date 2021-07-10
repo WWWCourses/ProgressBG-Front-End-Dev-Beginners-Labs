1c8bc5b7cd96da5bbb7def38f7b27ced65b270bb
WWWCourses <ivawebcourses@gmail.com>
2021-07-10 16:20:42 +0300

    Lab18: 10.07.2021 DONE: http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/JS_BasicSyntax_1/JS_BasicSyntax_1.html#/9/8

---
+++ lab18/basics.js
@@ -0,0 +1,104 @@
+// "use strict";
+// let a = 5
+// let b = 10;
+
+// res = a+b;
+
+// console.log(res);
+
+
+// RAM:
+// 0x12332: 0101 (5)
+// 0x12334: 1110 (10)
+// 0x12314: 01010 (15)
+
+// GLOBAL:
+// a 		=> 0x12332
+// b 		=> 0x12334
+// res		=> 0x12314
+
+// lower Camel Case
+// let userName = 'Ada';
+
+// Camel Case
+// let UserLastName = 'Byron';
+
+// snake-case
+// let user_last_name = 'Test';
+
+
+// console.log( userName);
+
+
+// let x = 5;
+// let y = 5.0;
+// let z = "5";
+
+// console.log( "5" + 5 );
+
+// 01010000000	=> x
+// 10101010101  => y
+// 10101111101  => z
+
+// console.log( 5 );
+// console.log( +5 );
+// console.log( 3 - -5 );
+// console.log( .5 );
+// console.log( 0.5 );
+// console.log( 1_200_000_123_233 );
+
+
+/* --------------------------------- Strings -------------------------------- */
+
+// let x = 5;
+
+// let tmp = '\tLine 1\n\tLine 2\n\tLine 3\n';
+// console.log(tmp);
+
+// console.log( 'Tom\'s pub' );
+// console.log( "a\\b" ); //
+
+
+// RAM:
+// 	01010000 (5)
+// 	111010101 ('5')
+
+// let lyrics = "В късна нощ - преди години - сам над книгите старинни,\wnбезотраден, вниквах, жаден, в знания незнайни тук; -\nскръбен, търсех без сполука мир във тайната наука -\nно оборен в сънна скука, чух внезапно бавен звук."
+
+// let lyrics = "В късна нощ - преди години - сам над книгите старинни,\n" +
+//        "безотраден, вниквах, жаден, в знания незнайни тук; -\n" +
+//        "скръбен, търсех без сполука мир във тайната наука -\n" +
+//        "но оборен в сънна скука, чух внезапно бавен звук.";
+
+// let newLyrics = `В късна нощ - преди години - сам над книгите старинни,
+//        безотраден, вниквах, жаден, в знания незнайни тук; -
+// скръбен, търсех без сполука мир във тайната наука -
+//        но оборен в сънна скука, чух внезапно бавен звук.`;
+
+
+// console.log( lyrics );
+// console.log( newLyrics );
+
+
+
+
+
+let userName = 'Pesho';
+let userAge = 23;
+
+
+
+// console.log( "I'm " + userName + ", and I'm " + userAge + " years old" );
+// console.log( "I'm " , userName , ", and I'm " ) ;
+
+
+// CTRL+SHIFT+B
+
+
+
+// Task:
+// "I'm Ada, and I'm 23 years old"
+
+console.log(`2+4 = ${userName}` );
+
+
