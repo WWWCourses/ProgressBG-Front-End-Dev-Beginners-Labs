94b6cb1426f268d5e5770e04ee9a5e5088f9dab3
WWWCourses <ivawebcourses@gmail.com>
2023-03-11 13:34:14 +0200

    Lab7: 11.03.2023 DONE: http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/CSS_Intro/CSS_Intro.html#/6/4

---
+++ lab7/back-end/registration.php
@@ -0,0 +1,10 @@
+name=`DROP table USERS&
+gender=
+gender=on
+gift=beer&gift=cola, water
+town=burgas
+USERS:
+
+name| pass  | gender | gift     	| town
+ada | 123 	| f      |  beer        | sofia
+pesho | 123 | m      | beer&cola
