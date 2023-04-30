26f2979db4d85bf969f55e502ff21da8ce7b6ff5
WWWCourses <ivawebcourses@gmail.com>
2023-04-30 13:36:20 +0300

    Lab20: 30.04.2023 DONE: JS_DOM_BOM

---
+++ lab20/DOM_intro.js
@@ -0,0 +1,39 @@
+// let window = {
+// 	'alert': function () {
+// 		console.log(`Alert`);
+// 	},
+// 	'document':{
+// 		'location': {
+// 			'host': "127.0.0.1:40101",
+
+// 		},
+// 		'body': {
+// 			'children': [1,2,3]
+// 		}
+// 	},
+// 	'Math': {
+// 		'PI': 3.145,
+// 		'min': function() {
+
+// 		}
+// 	}
+// }
+// let Math = window.Math;
+// let document = window.document;
+
+
+
+// console.log( window.document.body.children[1]);
+// console.dir(Math);
+// console.dir(window)
+// console.dir(global)
+
+
+// console.dir(window.Math.min(4,3,1));
+// console.dir(Math.min(4,3,1));
+
+
+// console.log( document );
+// console.dir( document );
+
+// console.dir(console);
