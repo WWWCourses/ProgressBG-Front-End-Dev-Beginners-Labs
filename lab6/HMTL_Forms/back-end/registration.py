4708679275b45a3f7b5c8254dddb7ab85d387397
WWWCourses <ivawebcourses@gmail.com>
2023-10-29 13:40:32 +0200

    Lab6: 29.10.2023 DONE: HTML_InputForms

---
+++ lab6/HMTL_Forms/back-end/registration.py
@@ -0,0 +1,17 @@
+
+INSERT into Users (UserName,Pass)
+
+if registration ok:
+	return "<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>index </title>
+	</head>
+	<body>
+		<h1>Welcome, Ada!</h1>
+	</body>
+	</html>
+	"
+
